f69bf919a2c823dd5dc8646ae2b06d6ea0734f5d
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-02-21 15:15:28 +0530

    21-02-2023 15:15
    Added Authentication

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const md5 = require("md5");
 
 const app = express();
 
@@ -10,6 +11,19 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
     extended : true
 }));
+mongoose.set('strictQuery', false);         // getting rid of warning
+
+mongoose.connect("mongodb://localhost:27017/assignmentSubmissionSystem");
+
+const userSchema = mongoose.Schema({
+    role: String, // "student", "faculty", or "hod"
+    name: String,
+    email: String,
+    password: String,
+    classrooms: [], // list of classrooms for faculty and hod
+    enrolledClasses: [] // list of classrooms for students
+});
+const USER = new mongoose.model("User",userSchema);
 
 app.get("/",function(req,res) {
     res.render("homepage");
@@ -21,7 +35,49 @@ app.get("/register",function(req,res) {
 
 app.post("/register",function(req,res) {
     console.log(req.body);
-    res.send(req.body);
+    const tempUser = new USER({
+        name : req.body.newName,
+        email : req.body.newEmail,
+        password : md5(req.body.newPassword),
+        role : req.body.newRole
+    });
+    tempUser.save(function(err) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            res.send("<h1>Registration Successful. You can now login to your account.</h1>")
+        }
+    });
+});
+
+app.post("/login",function(req,res) {
+    console.log(req.body,md5(req.body.password));
+    USER.findOne({email : req.body.email},function(err,foundUser) {
+        if(foundUser.password === md5(req.body.password)) {
+            if(foundUser.role === "student") {
+                res.render("student-dashboard",{
+                    NAME : foundUser.name,
+                    ROLE : foundUser.role
+                });
+            }
+            else if(foundUser.role === "faculty") {
+                res.render("faculty-dashboard",{
+                    NAME : foundUser.name,
+                    ROLE : foundUser.role
+                });
+            }
+            else if(foundUser.role === "hod") {
+                res.render("hod-dashboard",{
+                    NAME : foundUser.name,
+                    ROLE : foundUser.role
+                });
+            }
+        }
+        else {
+            res.send("Login failed. Try to login again by entering correct credentials.");
+        }
+    });
 });
 
 app.listen(3000,function() {
