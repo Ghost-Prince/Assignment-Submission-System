c2d2b8e417890c3e01641840db67d7a37070efe8
Adeeb Jamal <adeeb.jamal186@outlook.com>
2023-02-21 21:55:22 +0530

    21-02-2023 21:55
    Added feature to create classroom

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.set('strictQuery', false);         // getting rid of warning
 mongoose.connect("mongodb://localhost:27017/assignmentSubmissionSystem");
 
 const userSchema = mongoose.Schema({
+    ID: Number,
     role: String, // "student", "faculty", or "hod"
     name: String,
     email: String,
@@ -25,6 +26,16 @@ const userSchema = mongoose.Schema({
 });
 const USER = new mongoose.model("User",userSchema);
 
+const classSchema = mongoose.Schema({
+    ID: Number,
+    name: String,
+    description: String,
+    facultyId: Number, // ID of the faculty who created the classroom
+    students: [], // list of student IDs enrolled in the classroom
+    assignments: [] // list of assignment IDs posted in the classroom
+});
+const CLASS = new mongoose.model("Class",classSchema);
+
 app.get("/",function(req,res) {
     res.render("homepage");
 });
@@ -33,28 +44,42 @@ app.get("/register",function(req,res) {
     res.render("register");
 });
 
+app.get("/create-classroom",function(req,res) {
+    res.render("create-classroom");
+});
+
 app.post("/register",function(req,res) {
-    console.log(req.body);
-    const tempUser = new USER({
-        name : req.body.newName,
-        email : req.body.newEmail,
-        password : md5(req.body.newPassword),
-        role : req.body.newRole
-    });
-    tempUser.save(function(err) {
+    USER.countDocuments({},function(err,count) {
         if(err) {
             res.send(err);
         }
         else {
-            res.send("<h1>Registration Successful. You can now login to your account.</h1>")
+            const tempUser = new USER({
+                ID : count + 1,
+                name : req.body.newName,
+                email : req.body.newEmail,
+                password : md5(req.body.newPassword),
+                role : req.body.newRole
+            });
+            tempUser.save(function(err) {
+                if(err) {
+                    res.send(err);
+                }
+                else {
+                    res.send("<h1>Registration Successful. You can now login to your account.</h1>");
+                }
+            });
         }
     });
 });
+
+var loggedInUser = "";
 
 app.post("/login",function(req,res) {
     console.log(req.body,md5(req.body.password));
     USER.findOne({email : req.body.email},function(err,foundUser) {
         if(foundUser.password === md5(req.body.password)) {
+            loggedInUser = req.body.email;
             if(foundUser.role === "student") {
                 res.render("student-dashboard",{
                     NAME : foundUser.name,
@@ -63,8 +88,7 @@ app.post("/login",function(req,res) {
             }
             else if(foundUser.role === "faculty") {
                 res.render("faculty-dashboard",{
-                    NAME : foundUser.name,
-                    ROLE : foundUser.role
+                    NAME : foundUser.name
                 });
             }
             else if(foundUser.role === "hod") {
@@ -76,6 +100,50 @@ app.post("/login",function(req,res) {
         }
         else {
             res.send("Login failed. Try to login again by entering correct credentials.");
+        }
+    });
+});
+
+app.post("/create-classroom",function(req,res) {
+    CLASS.countDocuments(function(err,count) {
+        if(err) {
+            res.send(err);
+        }
+        else {
+            USER.findOne({email : req.body.classFacultyEmail},function(err,foundUser) {
+                if(err) {
+                    res.send(err); 
+                }
+                else {
+                    const tempClass = new CLASS({
+                        ID : count + 1,
+                        name : req.body.className,
+                        description : req.body.classDescription,
+                        facultyId : Number(foundUser.ID)
+                    });
+                    tempClass.save(function(err) {
+                        if(err) {
+                            res.send(err);
+                        }
+                        else {
+                            USER.updateOne(
+                                {email : req.body.classFacultyEmail},
+                                {
+                                    $push : {classrooms : tempClass.ID}
+                                },
+                                function(err) {
+                                    if(err) {
+                                        res.send(err);
+                                    }
+                                    else {
+                                        res.send("Classroom created successfully.");
+                                    }
+                                }
+                            );
+                        }
+                    });
+                }
+            });
         }
     });
 });
