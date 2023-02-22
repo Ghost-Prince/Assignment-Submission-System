242862924189560e948bbc2780070415d05d0106
skfrost19 <saahhiill123456@gmail.com>
2023-02-22 21:06:40 +0530

    database schema added

--- app.js
+++ app.js
@@ -3,83 +3,85 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const md5 = require("md5");
-
 const app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-    extended : true
-}));
-mongoose.set('strictQuery', false);         // getting rid of warning
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+mongoose.set("strictQuery", false); // getting rid of warning
 
-mongoose.connect("mongodb://localhost:27017/assignmentSubmissionSystem");
+mongoose.connect("mongodb://127.0.0.1:27017/assignmentSubmissionSystem");
 
 const userSchema = mongoose.Schema({
-    role: String, // "student", "faculty", or "hod"
-    name: String,
-    email: String,
-    password: String,
-    classrooms: [], // list of classrooms for faculty and hod
-    enrolledClasses: [] // list of classrooms for students
-});
-const USER = new mongoose.model("User",userSchema);
-
-app.get("/",function(req,res) {
-    res.render("homepage");
+  role: String, // "student", "faculty", or "hod"
+  name: String,
+  email: String,
+  password: String,
+  classrooms: [], // list of classrooms for faculty and hod
+  enrolledClasses: [], // list of classrooms for students
 });
 
-app.get("/register",function(req,res) {
-    res.render("register");
+const USER = new mongoose.model("User", userSchema);
+
+app.get("/", function (req, res) {
+  res.render("homepage");
 });
 
-app.post("/register",function(req,res) {
-    console.log(req.body);
-    const tempUser = new USER({
-        name : req.body.newName,
-        email : req.body.newEmail,
-        password : md5(req.body.newPassword),
-        role : req.body.newRole
-    });
-    tempUser.save(function(err) {
-        if(err) {
-            res.send(err);
-        }
-        else {
-            res.send("<h1>Registration Successful. You can now login to your account.</h1>")
-        }
-    });
+app.get("/register", function (req, res) {
+  res.render("register");
 });
 
-app.post("/login",function(req,res) {
-    console.log(req.body,md5(req.body.password));
-    USER.findOne({email : req.body.email},function(err,foundUser) {
-        if(foundUser.password === md5(req.body.password)) {
-            if(foundUser.role === "student") {
-                res.render("student-dashboard",{
-                    NAME : foundUser.name,
-                    ROLE : foundUser.role
-                });
-            }
-            else if(foundUser.role === "faculty") {
-                res.render("faculty-dashboard",{
-                    NAME : foundUser.name,
-                    ROLE : foundUser.role
-                });
-            }
-            else if(foundUser.role === "hod") {
-                res.render("hod-dashboard",{
-                    NAME : foundUser.name,
-                    ROLE : foundUser.role
-                });
-            }
-        }
-        else {
-            res.send("Login failed. Try to login again by entering correct credentials.");
-        }
-    });
+app.post("/register", function (req, res) {
+  console.log(req.body);
+  const tempUser = new USER({
+    name: req.body.newName,
+    email: req.body.newEmail,
+    password: md5(req.body.newPassword),
+    role: req.body.newRole,
+  });
+  tempUser.save(function (err) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(
+        "<h1>Registration Successful. You can now login to your account.</h1>"
+      );
+    }
+  });
 });
 
-app.listen(3000,function() {
-    console.log("Server is running on port 3000.");
+app.post("/login", function (req, res) {
+  console.log(req.body, md5(req.body.password));
+  USER.findOne({ email: req.body.email }, function (err, foundUser) {
+    if (foundUser.password === md5(req.body.password)) {
+      if (foundUser.role === "student") {
+        res.render("student-dashboard", {
+          NAME: foundUser.name,
+          ROLE: foundUser.role,
+        });
+      } else if (foundUser.role === "faculty") {
+        res.render("faculty-dashboard", {
+          NAME: foundUser.name,
+          ROLE: foundUser.role,
+        });
+      } else if (foundUser.role === "hod") {
+        res.render("hod-dashboard", {
+          NAME: foundUser.name,
+          ROLE: foundUser.role,
+        });
+      }
+    } else {
+      res.send(
+        "Login failed. Try to login again by entering correct credentials."
+      );
+    }
+  });
+});
+
+app.listen(3000, function () {
+  console.log("Server is running on port 3000.");
 });
